f6726569943ea9b55973694250b7f928e4268096
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-10 23:50:11 +0530

    Added: Adding Program to Create Stack.

---
+++ src/com/bridglablz/DataStructure.java
@@ -0,0 +1,9 @@
+package com.bridglablz;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        //System.out.println("Welcome to Stacks");
+        Stack newStack=new Stack();
+        newStack.push();
+    }
+}
---
+++ src/com/bridglablz/Stack.java
@@ -0,0 +1,18 @@
+package com.bridglablz;
+
+public class Stack {
+    public Node top;
+    LinkedList linkedList;
+
+    public Stack() {
+        this.linkedList = new LinkedList();
+    }
+    public void push() {
+        linkedList.push(70);
+        linkedList.push(30);
+        top = linkedList.push(56);
+        linkedList.print();
+        System.out.println("Top -> " + top.data);
+    }
+}
+
